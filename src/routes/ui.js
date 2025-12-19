89a5e93d1d7245981bdc34673377ba909ac7ff8a
akashjaiswal36 <akash.jaiswal36@gmail.com>
2025-12-19 12:09:59 +0530

    added dockerhub

--- src/routes/ui.js
+++ src/routes/ui.js
@@ -5,7 +5,7 @@ router.get('/', (req, res) => {
   res.send(`
     <html>
       <head>
-        <title>DevOps Platform Demo Project-1</title>
+        <title>DevOps Platform Demo i'm from DockerHub</title>
         <style>
           body { font-family: Arial; background: #eef2f7; padding: 40px; }
           .card {
@@ -19,7 +19,7 @@ router.get('/', (req, res) => {
       </head>
       <body>
         <div class="card">
-          <h2>🚀 DevOps Platform Demo Project-1</h2>
+          <h2>🚀 DevOps Platform Demo i'm from DockerHub</h2>
           <p><b>Status:</b> UP</p>
           <p><b>Version:</b> ${process.env.APP_VERSION || 'v1'}</p>
           <p><b>Environment:</b> ${process.env.ENV || 'dev'}</p>
