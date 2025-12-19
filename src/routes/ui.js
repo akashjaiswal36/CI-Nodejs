380de44471e0ed697fc9b73ee6fa996dcaaf15c9
akashjaiswal36 <68245389+akashjaiswal36@users.noreply.github.com>
2025-12-20 04:30:18 +0530

    Update ui.js

--- src/routes/ui.js
+++ src/routes/ui.js
@@ -19,7 +19,7 @@ router.get('/', (req, res) => {
       </head>
       <body>
         <div class="card">
-          <h2>🚀 DevOps Platform Demo, I am from Local workflow WORKFLOW..</h2>
+          <h2>🚀 DevOps Platform Demo, I am from Local workflow WORKFLOW....</h2>
           <p><b>Status:</b> UP</p>
           <p><b>Version:</b> ${process.env.APP_VERSION || 'v1'}</p>
           <p><b>Environment:</b> ${process.env.ENV || 'dev'}</p>
