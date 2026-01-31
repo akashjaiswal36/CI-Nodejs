bc3dc52f273adc70ac96a5496da415fc8c4921ad
akashjaiswal36 <68245389+akashjaiswal36@users.noreply.github.com>
2026-01-31 22:40:30 +0530

    Update ui.js

--- src/routes/ui.js
+++ src/routes/ui.js
@@ -19,7 +19,7 @@ router.get('/', (req, res) => {
       </head>
       <body>
         <div class="card">
-          <h2>🚀 DevOps Platform Demo, Updated value.yaml.WTFFFFFFFF</h2>
+          <h2>🚀 DevOps Platform Demo, Updated value.yaml.NEED TO DO IT AT ANY COST</h2>
           <p><b>Status:</b> UP</p>
           <p><b>Version:</b> ${process.env.APP_VERSION || 'v1'}</p>
           <p><b>Environment:</b> ${process.env.ENV || 'dev'}</p>
